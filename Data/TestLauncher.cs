7928c488754c98a8d0b3abd114d2e547f024d663
David Purdy <Purdydj@Rose-Hulman.edu>
2020-08-07 17:04:28 -0400

    change with test status

--- Data/TestLauncher.cs
+++ Data/TestLauncher.cs
@@ -5,35 +5,28 @@ namespace BlazorApp.Data
 {
     public class TestLauncher
     {
-        public bool InProgress = true;
         public void Runner(string tests)
         {
-            InProgress = true;
-            ProcessStartInfo cmdStartInfo = new ProcessStartInfo();
-            cmdStartInfo.FileName = @"C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe";
-            cmdStartInfo.RedirectStandardOutput = true;
-            cmdStartInfo.RedirectStandardError = true;
-            cmdStartInfo.RedirectStandardInput = true;
-            cmdStartInfo.UseShellExecute = false;
-            //Change to false to see what is going on or open debug console. 
-            cmdStartInfo.CreateNoWindow = true;
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = @"C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe";
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.RedirectStandardInput = true;
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
 
-            Process cmdProcess = new Process();
-            cmdProcess.StartInfo = cmdStartInfo;
-            cmdProcess.ErrorDataReceived += cmd_Error;
-            cmdProcess.OutputDataReceived += cmd_Output;
-            cmdProcess.EnableRaisingEvents = true;
-            cmdProcess.Start();
-            cmdProcess.BeginOutputReadLine();
-            cmdProcess.BeginErrorReadLine();
-            //Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\vsdevcmd.bat
-            //Sets it to a Dev Prompt to be able to run mstest or vstest
-            cmdProcess.StandardInput.WriteLine("cd " + '"' + ConfigurationManager.AppSettings.Get("Vstest.exe") + '"');
-            cmdProcess.StandardInput.Write(@".\" + "vstest.console.exe ");
-            cmdProcess.StandardInput.WriteLine('"' + ConfigurationManager.AppSettings.Get("TestLocation") + '"' + " /Tests:" + tests);
-            cmdProcess.StandardInput.WriteLine("exit");
-            //cmdProcess.WaitForExit();
-            InProgress = false;
+            Process shellProcess = new Process();
+            shellProcess.StartInfo = startInfo;
+            shellProcess.ErrorDataReceived += cmd_Error;
+            shellProcess.OutputDataReceived += cmd_Output;
+            shellProcess.EnableRaisingEvents = true;
+            shellProcess.Start();
+            shellProcess.BeginOutputReadLine();
+            shellProcess.BeginErrorReadLine();
+            shellProcess.StandardInput.WriteLine("cd " + '"' + ConfigurationManager.AppSettings.Get("Vstest.exe") + '"');
+            shellProcess.StandardInput.Write(@".\" + "vstest.console.exe ");
+            shellProcess.StandardInput.WriteLine('"' + ConfigurationManager.AppSettings.Get("TestLocation") + '"' + " /Tests:" + tests);
+            shellProcess.StandardInput.WriteLine("exit");
         }
 
         public void cmd_Output(object sender, DataReceivedEventArgs e)
